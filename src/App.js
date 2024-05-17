d9b7a4e794c40c334b10259afde3bb6b4dcdd91a
Brandon Gonzalez <brajgon@gmail.com>
2024-05-16 20:33:17 -0600

    added state and effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import Header from '.components/Header'
+import { useEffect, useState } from 'react';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
+
+  const [movie, setMovie] = useState("")
+  const [watchlist, setWatchlist] = useState("")
+  const [page, setPage] = useState(1)
+
   return (
     <div className="App">
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,11 @@
-<header>
-   <h2>Brandon's Movie App</h2>
-</header>
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h2>Brandon's Movie App</h2>
+        </div>
+    )
+}
+
+export default Header;
