3fb039e86176a1e13436d4fe504a17bd2d395bdd
Brandon Gonzalez <brajgon@gmail.com>
2024-05-23 20:49:53 -0600

    final commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,7 +18,6 @@ const button = inWatchlist.length === 0 ? (
         <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
         <h3>{movie.original_title}</h3>
       </div>
-      {/* <button onClick={() => addMovie(movie)}>Add to List</button> */}
       {button}
     </div>
   );
